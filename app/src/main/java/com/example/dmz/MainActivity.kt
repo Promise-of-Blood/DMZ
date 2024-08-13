2483260da1b439e28a73d28f7cf95704671993bf
kdbswo <kdbswo@gmail.com>
2024-08-13 17:40:22 +0900

    Feature: Bottom Navigation Bar

--- app/src/main/java/com/example/dmz/MainActivity.kt
+++ app/src/main/java/com/example/dmz/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.dmz
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.dmz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+
+
     }
 }
