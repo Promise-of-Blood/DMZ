48cf49bf3d7121686d4e9c549f0e4be6e51299bb
kdbswo <kdbswo@gmail.com>
2024-08-14 14:23:23 +0900

    Feature: viewpager2에 적용할 Search Recent Adapter 추가

---
+++ app/src/main/java/com/example/dmz/ui/SearchRecentAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.dmz.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.dmz.R
+import com.example.dmz.databinding.ItemRecentSearchBinding
+import com.example.dmz.model.Search
+
+class SearchRecentAdapter(
+    private val searchList: List<Search>
+) : RecyclerView.Adapter<SearchRecentAdapter.SearchHolder>() {
+    class SearchHolder(val itemLayoutBinding: ItemRecentSearchBinding) :
+        RecyclerView.ViewHolder(itemLayoutBinding.root)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): SearchRecentAdapter.SearchHolder {
+        val itemLayoutBinding = DataBindingUtil.inflate<ItemRecentSearchBinding>(
+            LayoutInflater.from(parent.context), R.layout.item_recent_search, parent, false
+        )
+        return SearchHolder(itemLayoutBinding)
+    }
+
+    override fun onBindViewHolder(holder: SearchHolder, position: Int) {
+        holder.itemLayoutBinding.search = searchList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return searchList.size
+    }
+
+
+}
