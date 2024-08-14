b95faf6783f6ff199b37dd2134d59173fb4a94f3
idkos <leegang31@gmail.com>
2024-08-14 11:18:29 +0900

    fix: HomeFragment 적용 에러 해결

--- app/src/main/java/com/example/dmz/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/dmz/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.dmz.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,9 +23,10 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         setupViewPager()
-
+        Log.d("HomeFragment", "onCreateView")
         return binding.root
     }
+
 
     private fun setupViewPager() {
         val images = listOf(R.drawable.img_keyword_yoajung, R.drawable.img_keyword_yoajung, R.drawable.img_keyword_yoajung)
