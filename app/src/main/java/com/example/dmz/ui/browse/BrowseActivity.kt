8e00d4626622e0aec0c32c61f21d65cdeff47257
Orinugoori <bigbangok@naver.com>
2024-08-16 10:48:42 +0900

    Feature: lifestyle 모션 추가

--- app/src/main/java/com/example/dmz/ui/browse/BrowseActivity.kt
+++ app/src/main/java/com/example/dmz/ui/browse/BrowseActivity.kt
@@ -1,16 +1,17 @@
-package com.example.dmz
+package com.example.dmz.ui.browse
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dmz.R
 
-class TestActivity : AppCompatActivity() {
+class BrowseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.activity_browse)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
