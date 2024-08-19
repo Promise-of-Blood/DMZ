e661e825cb9407a76d26c82e21b2a42961dbe3fe
idkos <leegang31@gmail.com>
2024-08-19 21:16:06 +0900

    Add: recentTrend 프로퍼티 추가

---
+++ app/src/main/java/com/example/dmz/data/model/Keywords.kt
@@ -0,0 +1,7 @@
+package com.example.dmz.data.model
+
+data class Keywords(
+    val keyText: String,
+    val keyImage: Int,
+    val recentTrend: List<Int>
+)
--- app/src/main/java/com/example/dmz/ui/home/Keyword.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dmz.ui.home
-
-data class Keyword(
-    val keyImage: Int,
-    val keyText: String
-)
