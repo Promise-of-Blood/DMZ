755f737eaa118dad235b8a36911e5299e56fefee
taeyoung <rlaxodud911@gmail.com>
2024-08-20 18:50:15 +0900

    Refactor: 불필요한 코드 삭제

--- app/src/main/java/com/example/dmz/ui/quiz/StartFragment.kt
+++ app/src/main/java/com/example/dmz/ui/quiz/StartFragment.kt
@@ -15,26 +15,12 @@ import com.example.dmz.data.repository.QuizRepositoryImpl
 import com.example.dmz.databinding.FragmentStartBinding
 import com.example.dmz.viewmodel.QuizViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [StartFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class StartFragment : Fragment() {
     private var _binding: FragmentStartBinding? = null
     private val binding get() = _binding!!
 
     private val quizRepository = QuizRepositoryImpl(CacheDataSource.getCacheDataSource())
     private val quizViewModel: QuizViewModel by activityViewModels()
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,14 +32,6 @@ class StartFragment : Fragment() {
             val navOptions =
                 NavOptions.Builder().setPopUpTo(R.id.navigation_quiz_result, true).build()
             findNavController().navigate(action, navOptions)
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
@@ -83,25 +61,5 @@ class StartFragment : Fragment() {
     private fun startQuiz() {
         val action = StartFragmentDirections.actionQuizStartToQuizQuestion()
         findNavController().navigate(action)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StartFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StartFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
