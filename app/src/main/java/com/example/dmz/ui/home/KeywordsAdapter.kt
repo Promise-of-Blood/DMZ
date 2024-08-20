5a6b5c69e214748c8ee6e110bd384e9c9802b871
idkos <leegang31@gmail.com>
2024-08-21 00:58:54 +0900

    Add: Trend Keyword 클릭시 관련 영상 검색결과 화면으로 이동

--- app/src/main/java/com/example/dmz/ui/home/KeywordsAdapter.kt
+++ app/src/main/java/com/example/dmz/ui/home/KeywordsAdapter.kt
@@ -12,6 +12,16 @@ import com.example.dmz.data.model.Keywords
 
 class KeywordAdapter(private val imageList: List<Keywords>) : RecyclerView.Adapter<KeywordAdapter.KeywordViewHolder>() {
 
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    private var itemClickListener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        itemClickListener = listener
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KeywordViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_keyword, parent, false)
         return KeywordViewHolder(view)
@@ -20,6 +30,10 @@ class KeywordAdapter(private val imageList: List<Keywords>) : RecyclerView.Adapt
     override fun onBindViewHolder(holder: KeywordViewHolder, position: Int) {
         val keyword = imageList[position]
         holder.bind(keyword)
+
+        holder.itemView.setOnClickListener {
+            itemClickListener?.onItemClick(it, position)
+        }
     }
 
     override fun getItemCount(): Int {
