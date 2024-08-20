339e72a8a7e8520685427179ab2eeed57c86dc48
taeyoung <rlaxodud911@gmail.com>
2024-08-21 06:03:39 +0900

    Fix: 퀴즈가 정말 랜덤으로 표시되게 수정

--- app/src/main/java/com/example/dmz/ui/quiz/StartFragment.kt
+++ app/src/main/java/com/example/dmz/ui/quiz/StartFragment.kt
@@ -46,6 +46,7 @@ class StartFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 //        handleIsCompleted()
+        quizRepository.shuffleQuizList()
         initView()
         handleIsCollected()
     }
